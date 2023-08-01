3e1a4b24adfbda51b3d2df3c974ecc0d770a4cfa
Nana <tg623512@gmail.com>
2023-08-01 00:58:38 -0700

    feat:set up tailwindcss

--- my-portfolio/src/app/page.tsx
+++ my-portfolio/src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           >
             By{' '}
             <Image
-              src="/vercel.svg"
+              src="/tg623623nana.github.io/vercel.svg"
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
